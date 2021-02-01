4b6c01649906e4ada929eff063f9a9ad733d14a1
arpit1912 <arpitbandejiya@gmail.com>
2021-02-02 01:34:18 +0530

    added the player save state feature for the change of scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController Instance = null;
     private float speed = 10;
     private float GameSpeed = 8;
     private float movement = 0f;
     private float jump = 0f;
     private bool IsPressed = false;
     private Rigidbody2D rigidbody;
-    //private Animator playerAnimation;
-    // Start is called before the first frame update
+    
     void Start()
     {
-       rigidbody = GetComponent<Rigidbody2D> ();
-       //playerAnimation = GetComponent<Animator> ();     
-       rigidbody.velocity = new Vector2(GameSpeed,0);
+       
+       if (Instance != null)
+       {
            
+           Debug.Log("Object Already exist");
+           
+           Destroy(this.gameObject);
+           
+           GameObject go = GameObject.Find("player_sprite");
+           Vector3 pos = go.transform.position;
+           pos.x = PlayerPrefs.GetFloat("playerX");
+           pos.y = PlayerPrefs.GetFloat("playerY");
+           go.transform.position = pos;
+           return;
+       }
+
+       Instance = this;
+       Debug.Log("Hello");
+       GameObject.DontDestroyOnLoad(this.gameObject);
+       rigidbody = GetComponent<Rigidbody2D> ();
+       rigidbody.velocity = new Vector2(GameSpeed,0);
     }
 
     // Update is called once per frame
