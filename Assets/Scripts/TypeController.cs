4e8f97ea6ea71386c9c600e6f80078b35b6982f0
arpit1912 <arpitbandejiya@gmail.com>
2021-02-04 01:16:37 +0530

    added the collected objet list in the Drop Scene

---
+++ Assets/Scripts/TypeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private static int type;    // Anyone should not be able to change it as it will create a mess otherwise.
+    void Start()
+    {
+        type = Random.Range(0, 2);  // Define the max based on the type of waste we have 
+        Debug.Log(type);
+    }
+
+    public static int getType()
+    {
+        return type;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
