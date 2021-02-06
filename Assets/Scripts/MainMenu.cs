59cba63937a1da034e5335b8f39fd2599dac6648
arpit1912 <arpitbandejiya@gmail.com>
2021-02-06 14:47:06 +0530

    added home screen animation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,20 +1,37 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-public Animator transition;
-
+    public float tranistionTime = 1f;
     public void PlayGame ()
     {
-        SceneManager.LoadScene("mainScene");
+        LoadNextLevel();
+        //SceneManager.LoadScene("transition scene");
+        
         Time.timeScale = 1f;
          //StartCoroutine(PlayGames());
          
     }
 
+    public Animator transition;
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+        
+        SceneManager.LoadScene("transition scene");
+    }
+    
     // IEnumerator PlayGames()
     // { 
     //     transition.SetTrigger("start");
