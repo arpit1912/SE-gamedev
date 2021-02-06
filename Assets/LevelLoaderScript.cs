78d6a041fd0f2c7e4c5650d18ff03d68afb0e758
arpit1912 <arpitbandejiya@gmail.com>
2021-02-06 16:15:13 +0530

    added the confirmation Ui for Main Scene

--- Assets/LevelLoaderScript.cs
+++ Assets/LevelLoaderScript.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class LevelLoaderScript : MonoBehaviour
 {
-   
-    
-    
+    //public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // anim.enabled = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
