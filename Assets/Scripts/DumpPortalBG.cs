cccd7d0478b3a499c4fb5e82ee53ff308ba60951
shreyansh <shreyansh3926@gmail.com>
2021-02-06 18:30:21 +0530

    edis : dumpportal scene

---
+++ Assets/Scripts/DumpPortalBG.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DumpPortalBG : MonoBehaviour
+{
+    public Image bg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if(TypeController.getType() == 0)
+        {
+            bg.color = new Color32(255,0,0,78);
+            return;
+        }
+        else if(TypeController.getType() == 1)
+        {
+            bg.color = new Color32(255,0,0,40);
+            return;
+        }
+        else if(TypeController.getType() == 2)
+        {
+            bg.color = new Color32(255,255,0,78);
+            return;
+        }
+        else if(TypeController.getType() == 3)
+        {
+            bg.color = new Color32(0,114,0,78);
+            return;
+        }
+        else if(TypeController.getType() == 4)
+        {
+            bg.color = new Color32(145,145,145,78);
+            return;
+        }
+        else if(TypeController.getType() == 5)
+        {
+            bg.color = new Color32(53,203,229,78);
+            return;
+        }
+        else if(TypeController.getType() == 6)
+        {
+            bg.color = new Color32(229,119,0,110);
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DumpPortalTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DumpPortalTimer : MonoBehaviour
+{
+    public Transform LoadingBar;
+    public Transform TextIndicator;
+    [SerializeField] private float startTime;
+    [SerializeField] private float display;
+    private int timeinterval = 15;
+    
+    
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        display = timeinterval - (Time.time - startTime);
+        Debug.Log (display);
+        TextIndicator.GetComponent<Text>().text = ((int)display).ToString();
+        LoadingBar.GetComponent<Image>().fillAmount =  (display/timeinterval);
+    }
+}
